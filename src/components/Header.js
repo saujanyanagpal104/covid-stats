e5d68c218578dcc3717b11af30a0361c949e3d98
saujanya <saujanyanagpal104@gmail.com>
2020-03-31 20:49:12 +0530

    auto-update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import Logo from '../assets/Logo.jpg'
 
 const Header = () => (
     <div className='header'>
         <div className='title-and-logo'>
-            <img src={Logo} alt='covid-19' />
-            <h1>COVID-19</h1>
+            <h1>COVID-19 Stats</h1>
         </div>
     </div>
 )
